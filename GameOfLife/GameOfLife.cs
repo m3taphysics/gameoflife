adf4965d3b30887b978aa4441f4e752a08f7563a
Mikhail Agapov <mikhail.agapov@decentraland.org>
2022-12-02 16:51:14 +0100

    CountNeighbors

--- GameOfLife/GameOfLife.cs
+++ GameOfLife/GameOfLife.cs
@@ -1,20 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GameOfLife;
 
 public class GameOfLife
 {
+    private const char AliveCell = '*';
+
     public string Process(string input)
     {
-        (int rows, int columns, string map) = GetInputAndSize(input);
-        
-        return map;
+        (int rows, int columns, List<string> lines) = GetInputAndSize(input);
+
+        var output = lines.ToList();
+
+        for (var row = 0; row < rows; row++)
+        {
+            for (var column = 0; column < columns; column++)
+            {
+                if (IsAlive(lines, row, column))
+                    continue;
+
+                if (CountNeighbors(lines, row, column, AliveCell) == 3)
+                {
+                    var str = output[row];
+                    var charArray = str.ToCharArray();
+                    charArray[column] = AliveCell;
+                    output[row] = new string(charArray);
+                }
+            }
+        }
+
+        return string.Join("\n", output) + "\n";
     }
 
-    private (int rows, int columns, string map) GetInputAndSize(string input)
+    private bool IsAlive(List<string> lines, int row, int column)
     {
-        List<string> lines = new (input.Split("\n"));
+        var character = lines[row][column];
+        return character == AliveCell;
+    }
+
+    private (int rows, int columns, List<string> lines) GetInputAndSize(string input)
+    {
+        List<string> lines = new (input.Split("\n", StringSplitOptions.RemoveEmptyEntries));
         int rows;
         int columns;
         if (InputHasSize(input))
@@ -38,27 +66,35 @@ public class GameOfLife
             rows = lines.Count;
             columns = lines[0].Length;
         }
-
-        string map = string.Join("\n", lines);
-        return (rows, columns, map);
+        
+        return (rows, columns, lines);
     }
 
     private bool InputHasSize(string input)
     {
-        return input[0] != '.' && input[0] != '*';
+        return input[0] != '.' && input[0] != AliveCell;
     }
 
-    private bool HasNeighbor(string input, int row, int columns)
+    private int CountNeighbors(List<string> lines, int row, int column, char compareTo)
     {
-        (int row, int col) N_Neighbor;
-        (int row, int col) NW_Neighbor;
-        (int row, int col) W_Neighbor;
-        (int row, int col) SW_Neighbor;
-        (int row, int col) S_Neighbor;
-        (int row, int col) SE_Neighbor;
-        (int row, int col) E_Neighbor;
-        (int row, int col) NE_Neighbor;
-        return false;
+        var rows = lines.Count;
+        var columns = lines[0].Length;
+
+        var counter = 0;
+        
+        for (var i = Math.Max(0, row - 1); i <= Math.Min(rows - 1, row); i++)
+        {
+            for (var j = Math.Max(0, column - 1); j <= Math.Min(columns - 1, column); j++)
+            {
+                if (i == row && j == column)
+                    continue;
+
+                if (compareTo == lines[i][j])
+                    counter++;
+            }
+        }
+
+        return counter;
     }
     
 }
